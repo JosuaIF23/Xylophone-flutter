a18bd65f4f19ceb4eab3d8423e79bcdcc43a39b9
JosuaIF23 <142556166+JosuaIF23@users.noreply.github.com>
2025-12-28 14:08:27 +0700

    Using Tyoe Func with Return to make the buildKey it's a button to make the Text button with input and required method input

--- lib/main.dart
+++ lib/main.dart
@@ -18,70 +18,32 @@ class XylophoneApp extends StatelessWidget {
     print('it got Pressed');
   }
 
+  Expanded buildKey(Color colors, int soundNumber) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () async {
+          playSound(soundNumber);
+        },
+        child: Container(color: colors),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Expanded(
-                child: TextButton(
-                  onPressed: () async {
-                    playSound(1);
-                  },
-                  child: Container(color: Colors.red),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () async {
-                    playSound(2);
-                  },
-                  child: Container(color: Colors.orange),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () async {
-                    playSound(3);
-                  },
-                  child: Container(color: Colors.yellow),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () async {
-                    playSound(4);
-                  },
-                  child: Container(color: Colors.green),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () async {
-                    playSound(5);
-                  },
-                  child: Container(color: Colors.teal),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () async {
-                    playSound(6);
-                  },
-                  child: Container(color: Colors.blue),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () async {
-                    playSound(7);
-                  },
-                  child: Container(color: Colors.purple),
-                ),
-              ),
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7),
             ],
           ),
         ),
